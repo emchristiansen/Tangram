0082f6a1d40268d4fbc460ec65e92b9bb7b7feef
Eric Martin Christiansen <ericmartinchristiansen@gmail.com>
2013-11-03 16:45:55 -0800

    moved some code to PipeUtil, created main types for system

--- src/Tangram.hs
+++ src/Tangram.hs
@@ -2,70 +2,57 @@ import Data.Array.Repa.IO.DevIL
 import Control.Monad
 import Data.Maybe
 import Pipes
-import qualified Pipes.Prelude as P
 import Pipes.Concurrent
 import Control.Concurrent (threadDelay)
 import Data.Monoid
-import Data.Either
 import Control.Concurrent.Async
 import ImageIO
+import PipeUtil
+  
+data Tangram = Tangram Image
+
+type ImageProducer = Producer Image IO ()
+
+type ImagePool = Pipe Image Image IO ()
+
+type TangramMaker = Pipe Image (Either Image Tangram) IO ()
+
+type DisplaySetter = Consumer Tangram IO ()
 
 -- Lazily reads all the images in a directory.
-directoryImageProvider :: FilePath -> Producer Image IO ()
-directoryImageProvider directory = do
+directoryImageProducer :: FilePath -> ImageProducer
+directoryImageProducer directory = do
   fileNames <- lift $ imageFilesInDirectory directory
   for (each fileNames) $ \fileName -> do
     maybeImage <- lift $ readImageSafe fileName
     each $ maybeToList maybeImage
 
-imagePool :: Pipe Image Image IO ()
-imagePool = do
-  image <- await
-  yield image
-
-data Tangram = Tangram Image
-
 -- This should be something like Pipe Image Tangram IO ()
-tangramMaker :: Pipe Image (Either Image Tangram) IO ()
-tangramMaker = do
+debugTangramMaker :: TangramMaker
+debugTangramMaker = forever $ do
   image <- await
-  yield $ Left image
-  --yield $ Right $ Tangram image
+  --yield $ Left image
+  yield $ Right $ Tangram image
 
---imagePasser :: Pipe Image Image IO ()
---imagePasser = undefined
-
-imageWriter :: Consumer Image IO ()
-imageWriter = forever $ do
-  image <- await
+debugDisplaySetter :: FilePath -> DisplaySetter
+debugDisplaySetter filePath = forever $ do
+  Tangram image <- await
   lift $ putStrLn "Writing"
-  lift $ removeIfExists fileName
-  lift $ runIL $ writeImage fileName image
-  lift $ threadDelay 2000000  -- Wait 2 seconds
- where fileName = "/home/eric/Downloads/fractal2.png"
+  --lift $ removeIfExists filePath
+  --lift $ runIL $ writeImage filePath image
+  lift $ threadDelay 1000000  -- Wait 2 seconds
 
-takeLeftPipe :: Pipe (Either a b) a IO ()
-takeLeftPipe = P.map (\x -> lefts [x]) >-> P.concat
-
-takeRightPipe :: Pipe (Either a b) b IO ()
-takeRightPipe = P.map (\x -> rights [x]) >-> P.concat
-
-displaySetter :: Consumer Tangram IO ()
-displaySetter = P.map (\(Tangram image) -> image) >-> imageWriter
-
-main :: IO ()
-main = do  
-  let provider = directoryImageProvider "/home/eric/Bitcasa/data/fractals"
-
+runSystem :: ImageProducer -> ImagePool -> TangramMaker -> DisplaySetter -> IO ()
+runSystem imageProducer imagePool tangramMaker displaySetter = do
   (imagePoolOutput, imagePoolInput) <- spawn Single
   (tangramFailureOutput, tangramFailureInput) <- spawn Unbounded
-  (tangramSuccessOutput, tangramSuccessInput) <- spawn Unbounded
+  (tangramSuccessOutput, tangramSuccessInput) <- spawn Single  
 
-  let fetchImages = provider >-> toOutput imagePoolOutput
+  let fetchImages = imageProducer >-> toOutput imagePoolOutput
 
-  let imageSource = fromInput imagePoolInput
-  let tangramSink = toOutput $ tangramFailureOutput <> tangramSuccessOutput
-  let makeTangrams = imageSource >-> imagePool >-> tangramMaker >-> tangramSink
+  let input = fromInput imagePoolInput
+  let output = toOutput $ tangramFailureOutput <> tangramSuccessOutput
+  let makeTangrams = input >-> imagePool >-> tangramMaker >-> output
 
   let recycleImages = fromInput tangramFailureInput >-> takeLeftPipe >-> toOutput imagePoolOutput
 
@@ -78,27 +65,15 @@ main = do
 
   mapM_ wait [a, b, c, d]
 
-  --runEffect displayTangrams
+  return ()
 
-  --forkIO $ do runEffect $ provider >-> toOutput imagePoolOutput
+main :: IO ()
+main = do  
+  let imageProducer = directoryImageProducer "/home/eric/Bitcasa/data/fractals"
+  let imagePool = cat
+  let tangramMaker = debugTangramMaker
+  let displaySetter = debugDisplaySetter "/home/eric/Downloads/tangram.png"
 
-  --let foo = imageSource >-> imagePool >-> tangramMaker >-> tangramSink
-  --do runEffect $ foo  
+  runSystem imageProducer imagePool tangramMaker displaySetter
 
-  --let foo2 = fromInput tangramImageInput >-> takeLeftPipe >-> toOutput imagePoolOutput
-  --runEffect foo2
-  
-  --let foo3 = fromInput tangramTangramInput >-> takeRightPipe >-> displaySetter
-
-  --let imagePoolRedirect = takeLeftPipe >-> toOutput imagePoolOutput
-  --let displaySetterRedirect = takeRightPipe >-> displaySetter
-  --let foo4 = imageSource >-> imagePool >-> tangramMaker >-> (imagePoolRedirect <> displaySetterRedirect)
-
-  --provider >-> imagePoolOutput
-
-
-  --let effect = provider >-> imagePool
-  --let maker = tangramMaker >-> splitP
-  --let consumer = imageWriter
-  --runEffect $ provider >-> consumer
   putStrLn "Done"
