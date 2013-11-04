a2f86325de7c04cc3bfe8a5b4fe7d4aaf23943e6
Eric Martin Christiansen <ericmartinchristiansen@gmail.com>
2013-11-03 19:04:36 -0800

    png transcoding

--- src/ImageIO.hs
+++ src/ImageIO.hs
@@ -1,32 +1,23 @@
 module ImageIO where
 
---import Data.Array.Repa.IO.DevIL
 import System.Directory
 import System.FilePath.Posix
 import Data.Char
-import Control.Exception
-import System.IO.Error
+--import Control.Exception
+--import System.IO.Error
 import Codec.Picture
 import Text.Printf
---import Data.Array.Repa
---import Data.Array.Repa.Repr.ForeignPtr
---import Data.Array.Repa.Index
---import Data.Word
+import Data.ByteString.Lazy (toStrict)
 
 -- https://www.fpcomplete.com/user/snoyberg/general-haskell/exceptions/catching-all-exceptions
-catchAny :: IO a -> (SomeException -> IO a) -> IO a
-catchAny = Control.Exception.catch
+--catchAny :: IO a -> (SomeException -> IO a) -> IO a
+--catchAny = Control.Exception.catch
 
-removeIfExists :: FilePath -> IO ()
-removeIfExists fileName = removeFile fileName `catch` handleExists
-  where handleExists e
-          | isDoesNotExistError e = return ()
-          | otherwise = throwIO e
-
---imageToRepa :: Image -> Array F DIM3 Word8
---imageToRepa image = case image of
---  RGBA (Array F DIM3 Word8) -> undefined
-
+--removeIfExists :: FilePath -> IO ()
+--removeIfExists fileName = removeFile fileName `catch` handleExists
+--  where handleExists e
+--          | isDoesNotExistError e = return ()
+--          | otherwise = throwIO e
 
 -- Checks if a filename is an image filename for some standard extensions.
 isImageFilename :: FilePath -> Bool
@@ -39,13 +30,16 @@ imageFilesInDirectory directory = do
   return $ map ((directory ++ "/") ++) $ filter isImageFilename allFilenames
 
 transcodeToPNG :: DynamicImage -> Maybe DynamicImage
-transcodeToPNG = undefined
+transcodeToPNG image = case encodeDynamicPng image of
+  Left _ -> Nothing
+  Right byteString -> case decodePng (toStrict byteString) of
+  	Left _ -> Nothing
+  	Right transcoded -> Just transcoded
 
 readImageSafe :: FilePath -> IO (Maybe DynamicImage)
 readImageSafe filePath = do
   putStrLn $ "Reading " ++ filePath
   eitherImage <- readImage filePath
-  --return eitherImage
   case eitherImage of
   	Right image -> case transcodeToPNG image of
   	  Nothing -> do
@@ -53,17 +47,6 @@ readImageSafe filePath = do
   	  	return Nothing
   	  transcoded -> return transcoded
   	Left errorMessage -> do
-  	  putStrLn $ printf "Failed to read %s with error: %s.\nSkipping." filePath errorMessage
+  	  let formatString = "Failed to read %s with error: %s.\nSkipping."
+  	  putStrLn $ printf formatString filePath errorMessage
   	  return Nothing
-    
-
-
-  --case readImage filePath of
-  --	Left error -> do
-  --	  putStrLn $ "Could not read image, skipping: " ++ filePath
-  --	  return Nothing
-  --  Right image -> return $ Just image 
-
-  --catchAny (fmap Just (runIL (readImage filename))) $ \_ -> do
-  --	putStrLn $ "Could not read image, skipping: " ++ filename
-  --	return Nothing  
