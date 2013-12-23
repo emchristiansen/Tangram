78a0e9b8e075a8cffdec3bb6ac7c98729076eb0e
Eric Martin Christiansen <ericmartinchristiansen@gmail.com>
2013-12-22 21:18:58 -0800

    comment

--- src/TangramMaker.hs
+++ src/TangramMaker.hs
@@ -11,5 +11,8 @@ import Tree
 import ImageRGBA8
 import Util
 
+-- Consumes images and produces tangrams.
+-- When everything goes well, the output is a `Right Tangram`.
+-- Some images are rejects, which get returned as failures (`Left ImageRGBA8`).
 type TangramMaker = Pipe ImageRGBA8 (Either ImageRGBA8 Tangram) IO ()
 
